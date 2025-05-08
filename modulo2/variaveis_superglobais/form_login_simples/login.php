441b8cb3cc408c8deaa92b44f4d03bae43088efa
PabloSantos-CG <comercial-pablo@outlook.com>
2025-05-07 21:24:48 -0300

    resolves vulnerability in input type of select

--- modulo2/variaveis_superglobais/form_login_simples/login.php
+++ modulo2/variaveis_superglobais/form_login_simples/login.php
@@ -3,12 +3,13 @@
 if (!empty($_POST['login']) && !empty($_POST['password'])) {
     $login = htmlspecialchars($_POST['login']);
     $password = htmlspecialchars($_POST['password']);
+    $theme = htmlspecialchars($_POST['theme']);
 
     if ($login == 'admin' && $password == 'admin') {
         session_start();
         $_SESSION['login'] = $login;
         $_SESSION['password'] = $password;
-        $_SESSION['theme'] = $_POST['theme'];
+        $_SESSION['theme'] = $theme;
         header('Location: welcome.php');
     } else {
         $message = 'Não foi possível autenticar o usuário, tente novamente.';
